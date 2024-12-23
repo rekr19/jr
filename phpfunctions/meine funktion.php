82f701c9112ebe95c942617ac59a1be043b8acaa
rekr19 <rene.krumrueck@ikalro.de>
2024-12-23 12:08:11 +0100

    new file

---
+++ phpfunctions/meine funktion.php
@@ -0,0 +1,11 @@
+<?php
+
+class className extends JobRouter\Engine\Runtime\PhpFunction\RuleExecutionFunction
+{
+	public function execute($rowId = null)
+	{
+		// Hier die Funktionalität einfügen
+        $username;
+	}
+}
+?>
