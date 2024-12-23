a150c1354faf0e43f3e006cc19cb96f6955ca772
rekr19 <rene.krumrueck@ikalro.de>
2024-12-23 11:58:23 +0100

    new file

--- scripts/desktop/dlgStart/change.js
+++ scripts/desktop/dlgStart/change.js
@@ -1,4 +1,4 @@
 function showMessage(){
-    var rolle = jr_get_value('listZuweisung');
-    alert('Die Aufgabe wird an die Rolle ' + rolle + 'zugewiesen!');
+    var role = jr_get_value('listZuweisung');
+    alert('Die Aufgabe wird an die Rolle ' + role + 'zugewiesen!');
 }
